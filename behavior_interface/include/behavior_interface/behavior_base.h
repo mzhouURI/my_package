fccd92eeeaa27afd9415c46f5ab4b7898f46c3b3
Lin Zhao <linzhao@uri.edu>
2024-02-26 10:01:22 -0500

    template for plugin

---
+++ behavior_interface/include/behavior_interface/behavior_base.h
@@ -0,0 +1,190 @@
+#ifndef MVP2_MISSION__BEHAVIOR_BASE_
+#define MVP2_MISSION__BEHAVIOR_BASE_
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "mvp2_util/lifecycle_node.hpp"
+#include "tf2_ros/buffer.h"
+#include <mvp_msgs/msg/control_process.hpp>
+
+namespace mvp2_helm
+{
+
+class BehaviorBase {
+private:
+    friend class Helm;
+
+    friend class BehaviorContainer;
+
+    /**
+        * @brief Unique name for the behavior.
+        * This name will later be used as namespace for ros node handler.
+        *
+        * @todo A behavior may or may not use a parameter from ROS. However,
+        *       this name is still a necessity.
+        */
+    std::string m_name;
+
+    /**
+        * @brief Frequency of the helm
+        */
+    double m_helm_frequency;
+
+    /**
+        * @brief Global link of helm
+        */
+    std::string m_global_link;
+
+    /**
+        * @brief Local link of helm
+        */
+    std::string m_local_link;
+
+    /**
+        * @brief A string holds the active state name
+        */
+    std::string m_active_state;
+
+    /**
+        * @brief A vector holds DOFs active at the moment by controller
+        *
+        */
+    std::vector<int> m_active_dofs;
+
+    /**
+        * @brief Behaviors calls this function to request a state change from
+        *        MVP-Helm.
+        *
+        * This function is set during the runtime to map one of the functions
+        * from MVP-Helm.
+        */
+    std::function<bool(const std::string&)> f_change_state;
+
+    void f_set_active_state(const std::string& state) {
+        m_active_state = state;
+        state_changed(state);
+    }
+
+    /**
+        * @brief This function is called by the MVP-Helm everytime if a
+        *        behavior is active in the given state.
+        */
+    void f_activate()
+    {
+        if(!m_activated) {
+            activated();
+        }
+        m_activated = true;
+    }
+
+    /**
+        * @brief This function is called by the MVP-Helm everytime if a
+        *        behavior is *not* active in the given state.
+        */
+    void f_disable()
+    {
+        if(m_activated) {
+            disabled();
+        }
+        m_activated = false;
+    }
+
+protected:
+    /**
+        * @brief A vector holds controlled DOFs by behavior
+        * Each behavior must present the degrees of freedoms that they want
+        * to control. Helm will be controlling this information during
+        * execution.
+        */
+    std::vector<int> m_dofs;
+
+    /**
+        * @brief Registered state of the of the low level controller
+        */
+    mvp_msgs::msg::ControlProcess m_process_values;
+
+    /**
+        * @brief a member variable to hold activity state of the behavior
+        */
+    bool m_activated = false;
+
+    /**
+        * @brief This function is triggered when a behavior gets activated
+        * A plugin may or may not override this function.
+        */
+    virtual void activated() = 0;
+
+    /**
+        * @brief This function is triggered when a behavior gets disabled
+        * A plugin may or may not override this function.
+        */
+    virtual void disabled() = 0;
+
+    /**
+        * @brief
+        *
+        * @param state_name
+    */
+    virtual void state_changed(const std::string& state_name) {}
+
+    virtual auto change_state(const std::string& state) -> bool final {
+        return f_change_state(state);
+    }
+
+    virtual double get_helm_frequency() final { return m_helm_frequency; }
+
+    virtual std::string get_helm_global_link() final { return m_global_link; }
+
+    virtual std::string get_helm_local_link() final { return m_local_link; }
+
+    virtual auto configure_dofs() -> decltype(m_dofs) {return decltype(m_dofs)();};
+
+public:
+
+    /**
+        * @brief Construct a new Behavior Base object
+        *
+        */
+    BehaviorBase() = default;
+
+    virtual ~BehaviorBase() = default;    
+
+    /**
+        * @brief retrieve degrees of freedoms controlled by the behavior
+        *
+        * @return std::vector<ctrl::DOF::IDX>
+        */
+    virtual auto get_dofs() -> const decltype(m_dofs)& final
+    {
+        return m_dofs;
+    }
+
+    virtual auto get_name() -> std::string final { return m_name; }
+
+    /**
+        * @brief
+        * @param set_point
+        * @return true if Behavior wants helm to use its result
+        * @return false if Behavior doesn't want helm to use its result
+        */
+    virtual bool request_set_point(mvp_msgs::msg::ControlProcess* set_point) = 0;
+
+    /**
+    * @param  parent pointer to user's node
+    * @param  name The name of this planner
+    * @param  tf A pointer to a TF buffer
+    */
+    virtual void initialize(
+        const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent,
+        const std::string &name) = 0;
+
+    /**
+     * @brief Trivial generic pointer type
+     */
+    typedef std::shared_ptr<BehaviorBase> Ptr;
+};
+
+} // namespace mvp2_helm
+
+#endif  // MVP2_MISSION__BEHAVIOR_BASE_
---
+++ bhv_template/include/bhv_template/bhv_template.h
@@ -0,0 +1,89 @@
+#ifndef MVP2_MISSION__BEHAVIOR_TEMPLATE_
+#define MVP2_MISSION__BEHAVIOR_TEMPLATE_
+
+#include <memory>
+#include <string>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <thread>
+#include <utility>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/create_timer_ros.h"
+#include "geometry_msgs/msg/twist.hpp"
+// #include "nav2_util/simple_action_server.hpp"
+#include "mvp2_util/robot_utils.hpp"
+#include "behavior_interface/behavior_base.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include "tf2/utils.h"
+#pragma GCC diagnostic pop
+
+namespace mvp2_helm 
+{
+
+using namespace std::chrono_literals;  //NOLINT
+
+class BehaviorTemplate : public BehaviorBase
+{
+private:
+    /**
+        * @brief This function is triggered when a behavior gets activated
+        * A plugin may or may not override this function.
+        */
+    virtual void activated() override;
+
+    /**
+        * @brief This function is triggered when a behavior gets disabled
+        * A plugin may or may not override this function.
+        */
+    virtual void disabled() override;
+
+    /**
+    * @param  parent pointer to user's node
+    * @param  name The name of this planner
+    * @param  tf A pointer to a TF buffer
+    */
+    virtual void initialize(
+        const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent,
+        const std::string &name) override;
+
+    /**
+    * @brief Callback function for velocity subscriber
+    * @param msg received Twist message
+    */
+    void templateCallback(const std_msgs::msg::String::SharedPtr msg);
+
+    // ros2 related
+    rclcpp_lifecycle::LifecycleNode::WeakPtr m_node;
+    rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::String>::SharedPtr m_str_pub;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr m_str_sub;
+    rclcpp::Logger m_logger{rclcpp::get_logger("mvp2_mission")};
+
+    // variables
+    double m_test_double;
+
+public:
+    /**
+        * @brief Trivial constructor
+        */
+    BehaviorTemplate();
+
+    /**
+        * @brief Request set point from the behavior. It is consumed by helm.
+        *
+        * @param msg Result value of the behavior. This value is written by the
+        *            Behavior. Helm uses this variable to generate set_point
+        *            for the controller.
+        * @return true if you want helm to use the result.
+        * @return false if you don't want helm to use the result.
+        */
+    bool request_set_point(mvp_msgs::msg::ControlProcess *msg) override;    
+
+};
+
+} // namespace mvp2_helm
+
+#endif // MVP2_MISSION__BEHAVIOR_TEMPLATE_
